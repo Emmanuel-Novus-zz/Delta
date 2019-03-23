467be38b70dd6d5628beb3cf81f4c3df52bd28ff
Emmanuel M <38224811+EmmanuelDevM@users.noreply.github.com>
2019-03-23 11:05:31 +0100

    Delete test.js

--- commands/utilitaires/test.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js');
-
-exports.run = (client, message, args) => {
-    message.channel.send("<:true:551460100347396107> __**Le serveur est actuellement en ligne.**__")
-}
-
-module.exports.help = {
-    name: "test"
-}
